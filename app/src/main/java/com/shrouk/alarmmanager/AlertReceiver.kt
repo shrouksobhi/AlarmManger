146c9023ec6482ffb8611413553297013d5d8cb8
shrouksobhi <shrouksobhi99@gmail.com>
2022-11-07 16:06:50 +0200

    init

---
+++ app/src/main/java/com/shrouk/alarmmanager/AlertReceiver.kt
@@ -0,0 +1,27 @@
+package com.shrouk.alarmmanager
+
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.core.content.ContextCompat.getSystemService
+
+class AlertReceiver:BroadcastReceiver() {
+    private val NOTIFICATION_ID=0
+    private lateinit var notificationManager: NotificationManager
+
+
+    override fun onReceive(context: Context, intent: Intent?) {
+
+        notificationManager = getSystemService(
+            context,
+            NotificationManager::class.java
+        ) as NotificationManager
+
+        notificationManager.sendNotification("Alarm just fired","")
+            Log.d("Alarm Ring", "Alarm just fired")
+
+
+        }
+    }
---
+++ app/src/main/java/com/shrouk/alarmmanager/MainActivity.kt
@@ -0,0 +1,94 @@
+package com.shrouk.alarmmanager
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+import java.text.SimpleDateFormat
+import java.util.*
+import javax.xml.datatype.DatatypeConstants.HOURS
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var alarmtextview: TextView
+    private lateinit var btncancel: Button
+    private lateinit var btnstart: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        alarmtextview = findViewById(R.id.textView)
+        btncancel = findViewById(R.id.btncancel)
+        btnstart = findViewById(R.id.btnstartalarm)
+
+        btncancel.setOnClickListener {
+            cancrlAlarm()
+        }
+        btnstart.setOnClickListener {
+            val picker =
+                MaterialTimePicker.Builder()
+                    .setTimeFormat(TimeFormat.CLOCK_12H)
+                    .setHour(12)
+                    .setMinute(10)
+                    .setTitleText("Select Appointment time")
+                    .build()
+
+            picker.addOnPositiveButtonClickListener{
+                val selectedTime = Calendar.getInstance()
+                selectedTime[2022, 11, 7, picker.hour, picker.minute] = 0
+//                    selectedTime.set(HOURS,picker.hour)
+                val formattedTime = SimpleDateFormat("hh:mm:ss a", Locale.getDefault()).format(selectedTime.time)
+                Log.e("TIME",selectedTime.timeInMillis.toString())
+                alarmtextview.text="Alarm set to "+formattedTime
+                    setAlarm(selectedTime.timeInMillis)
+                }
+
+            picker.addOnNegativeButtonClickListener {
+                // call back code
+            }
+            picker.addOnCancelListener {
+                // call back code
+            }
+            picker.addOnDismissListener {
+                // call back code
+            }
+
+              picker.show(supportFragmentManager,"tag")    }
+    }
+
+    private fun setAlarm(timeInMillis: Long) {
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(this, AlertReceiver::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0)
+        alarmManager.setRepeating(
+            AlarmManager.RTC,
+            timeInMillis,
+            AlarmManager.INTERVAL_DAY,
+            pendingIntent
+        )
+        Toast.makeText(this, "Alarm is set", Toast.LENGTH_SHORT).show()
+    }
+
+
+    private fun cancrlAlarm() {
+    }
+}
+//private class MyAlarm : BroadcastReceiver() {
+//    override fun onReceive(
+//        context: Context,
+//        intent: Intent
+//    ) {
+//        Log.d("Alarm Ring", "Alarm just fired")
+//
+//    }
+//}
